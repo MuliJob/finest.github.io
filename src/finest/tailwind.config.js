9485fef3a3f252f04e75415253bb10651722f8d7
Muli Job <munyoki912@gmail.com>
2024-11-26 05:55:53 +0300

    Added whitenoise and collected staticfiles

--- src/finest/tailwind.config.js
+++ src/finest/tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     './templates/**/*.html',
-    './node_modules/flowbite/**/*.js',
+    './src/**/*.js',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
