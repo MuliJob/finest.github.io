3bf3da621ad7f0e21a604695595ef23a344b19d0
Muli Job <munyoki912@gmail.com>
2024-11-26 14:58:53 +0300

    Deleted cash file and removed the ajax functionality

--- src/staticfiles/js/main.js
+++ src/staticfiles/js/main.js
@@ -1,14 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
     NProgress.start();
     NProgress.done();
-
-    if (typeof $ !== 'undefined') {
-      $(document).ajaxStart(function () {
-          NProgress.start();
-      });
-
-      $(document).ajaxStop(function () {
-          NProgress.done();
-      });
-  }
 });
