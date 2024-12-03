8b2db27ebd603de40ffb96d5045ff4c88183aac1
Muli Job <munyoki912@gmail.com>
2024-12-03 16:35:57 +0300

    Added member registration functionality

--- src/members/views.py
+++ src/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
   if request.method == 'POST':
@@ -12,7 +13,7 @@ def login_user(request):
         messages.success(request, 'Login Successful.')
         return redirect('dashboard')
     else:
-      messages.success(request, 'There was an error logging you in. Please try again.')
+      messages.error(request, 'There was an error logging you in. Please try again.')
       return redirect('login')
   else:
     return render(request, 'auth/login.html', {})
@@ -21,3 +22,20 @@ def logout_user(request):
   logout(request)
   messages.success(request, 'Logged out Successful.')
   return redirect('login')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Registration successful.')
+                return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'auth/register.html', {'form': form})
