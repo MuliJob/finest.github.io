c0ac93ef830b1b007a483b0f1b60a0501795912a
Muli Job <munyoki912@gmail.com>
2024-12-09 22:46:27 +0300

    Added docstring comments

--- src/members/forms.py
+++ src/members/forms.py
@@ -1,12 +1,15 @@
+""" Forms """
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
+    """ Adding user registration form fields """
     email = forms.EmailField()
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
 
     class Meta:
+        """ Form fields """
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
