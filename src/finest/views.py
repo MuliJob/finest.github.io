b2e031e86a386cf21fd4833269736e6b9fc0ed9b
Muli Job <munyoki912@gmail.com>
2024-12-09 19:40:02 +0300

    Added functionality to display projects posted by a user and displaying a detail page of the website

--- src/finest/views.py
+++ src/finest/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import SubmittedWebsite
 from .forms import SubmittedWebsiteForm
 
 
@@ -22,17 +23,22 @@ def dashboard(request):
 
 @login_required
 def my_post(request):
-    title = 'My Posts'
+    title = 'MY POSTS'
+    user_posts = SubmittedWebsite.objects.filter(user=request.user)
     context = {
-      'title':title,
+      'title': title,
+      'user_posts': user_posts,
     }
     return render(request, 'user/my-posts.html', context)
 
 @login_required
-def my_post_detail(request):
+def my_post_detail(request, pk):
+    
     title = 'Website Details'
+    website = get_object_or_404(SubmittedWebsite, pk=pk, user=request.user)
     context = {
-      'title':title,
+      'title': title,
+      'website': website,
     }
     return render(request, 'user/website-detail.html', context)
 
@@ -60,7 +66,7 @@ def submit_website(request):
         form = SubmittedWebsiteForm()
 
     context = {
-        'title': 'Submit Website',
+        'title': 'SUBMIT WEBSITE',
         'form': form,
     }
     return render(request, 'user/submit-website.html', context)
