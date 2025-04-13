d13479b0055dda2260fd75208a7dbf0a8134cc7f
MuliJob <munyoki912@gmail.com>
2025-04-13 21:26:12 +0300

    updated allowed hosts

--- src/kristal/settings.py
+++ src/kristal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS'), config('ALLOWED_HOSTS_2')]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
